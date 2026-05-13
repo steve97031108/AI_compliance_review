e88cea6f3eb4d654b418f13ec15875527f6748fd
kai <yinxuekaipersonal@icloud.com>
2026-05-13 19:30:26 +0800

    创建主启动类

---
+++ src/main/java/com/scu/aicompliance/AiComplianceApplication.java
@@ -0,0 +1,12 @@
+package com.scu.aicompliance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiComplianceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiComplianceApplication.class, args);
+    }
+}
